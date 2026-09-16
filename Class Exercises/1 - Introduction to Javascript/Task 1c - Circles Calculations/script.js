3a8356ad461a6d22d8cedd8fc17478b83e68a4a5
Jude9320 <628jk70@canonslade.tbft.uk>
2026-09-16 18:14:34 +0000

    1 task 1C done

--- Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
@@ -1,21 +1,19 @@
 // Add event listener to the calculate button
 document.getElementById('calculateBtn').addEventListener('click', calculateCircle);
-
+let area = 0
+let circum = 0
+let radius = 0
 function calculateCircle() {
     // TODO:
     // 1. Get the radius value from the input field
+    radius = parseInt(document.getElementById("radius").value)
     // 2. Calculate:
     //    - Circumference using the formula: 2 * π * radius
+    circum = ((2) * (Math.PI) * (radius))
     //    - Area using the formula: π * radius^2
+    area = ((Math.PI) * (radius ** 2))
     // 3. Display both results in their respective span elements
     // Note: Use Math.PI for the value of π
+    document.getElementById("circumference").textContent = + circum
+    document.getElementById("area").textContent = + area
 }
-
-let radius = parseInt(document.getelementbyID("Enter the radius"))
-
-circum = (2 * Math.PI * radius)
-area = (Math.PI * (radius ** 2))
-
-console.log("The circumference is " + circum)
-console.log("The area is " + area)
-
