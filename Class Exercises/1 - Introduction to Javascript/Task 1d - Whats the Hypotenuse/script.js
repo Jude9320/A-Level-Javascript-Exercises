1ed9faa004aaf5545d028aa6807ba607792a7c99
Jude9320 <628jk70@canonslade.tbft.uk>
2026-09-16 18:44:53 +0000

    1 task 1D done

--- Class Exercises/1 - Introduction to Javascript/Task 1d - Whats the Hypotenuse/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1d - Whats the Hypotenuse/script.js
@@ -4,13 +4,12 @@ document.getElementById('calculateBtn').addEventListener('click', calculateHypot
 function calculateHypotenuse() {
     // TODO:
     // 1. Get the lengths of sides A and B from the input fields
+    let A = parseInt(document.getElementById("sideA").value)
+    let B = parseInt(document.getElementById("sideB").value)
     // 2. Calculate the hypotenuse using the Pythagorean theorem: c = √(a² + b²)
+    hyp = (Math.sqrt((A**2) + (B**2)))
     // 3. Display the result in the hypotenuse span element
+    document.getElementById("hypotenuse").textContent = + hyp
+
     // Note: Use Math.sqrt() for square root
 }
-let sideA = parseFloat(prompt(console.log("Enter the length of side A")))
-let sideB = parseFloat(prompt(console.log("Enter the length of side B")))
-
-let hyp = Math.sqrt((sideA ** 2) + (sideB ** 2))
-
-console.log("The length of the hypotenuse is " + hyp);
