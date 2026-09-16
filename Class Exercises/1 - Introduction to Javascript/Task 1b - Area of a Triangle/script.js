6930a6b96f7f1b87b32944e6e72c446e29a0ea22
Jude9320 <628jk70@canonslade.tbft.uk>
2026-09-16 17:46:15 +0000

    1 Task1b done

--- Class Exercises/1 - Introduction to Javascript/Task 1b - Area of a Triangle/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1b - Area of a Triangle/script.js
@@ -1,12 +1,13 @@
 // Add event listener to the calculate button
 document.getElementById('calculateBtn').addEventListener('click', calculateArea);
-
+let area = 0
 function calculateArea() {
     // TODO:
     // 1. Get the base and height values from the input fields
-    let base = parseInt(document.getElementById("base"))
-    let height = parseInt(document.getElementById("height"))
+    let base = parseInt(document.getElementById("base").value)
+    let height = parseInt(document.getElementById("height").value)
     // 2. Calculate the area using the formula: area = (base * height) / 2
-    area = (base * height) / 2
+    area = (((base) * (height)) / (2))
     // 3. Display the result in the area span element
-    document.getElementById("area").textContent = "Area: " + area }
+    document.getElementById("area").textContent = + area 
+}
