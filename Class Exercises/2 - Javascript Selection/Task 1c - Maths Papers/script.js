7bc4871c56b99aec0f19a6539deef1644d7d8ac0
Jude9320 <628jk70@canonslade.tbft.uk>
2026-09-16 09:26:10 +0000

    2 task 1c done

--- Class Exercises/2 - Javascript Selection/Task 1c - Maths Papers/script.js
+++ Class Exercises/2 - Javascript Selection/Task 1c - Maths Papers/script.js
@@ -10,10 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate if student passed or failed
 function calculateResult() {
     // TODO: Get the marks for all three papers
-    
+    let paper1 = parseInt(document.getElementById("paper1").value)
+    let paper2 = parseInt(document.getElementById("paper2").value)
+    let paper3 = parseInt(document.getElementById("paper3").value)
     // TODO: Calculate the total mark
-    
+    let total = (paper1) + (paper2) + (paper3)
     // TODO: Check if total is greater than 160
-    
+    if ((total) > (160)) {
+        mark = "Pass"
+    }else {
+        mark = "Fail"
+    }
     // TODO: Display the total mark and whether the student passed or failed
+    document.getElementById("result").textContent = "Your mark was a " + mark
+
 }
