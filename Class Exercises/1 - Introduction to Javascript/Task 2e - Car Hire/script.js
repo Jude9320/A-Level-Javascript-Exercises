ea25580745fbbad8508ff7419a80eda98d516bcc
Jude9320 <628jk70@canonslade.tbft.uk>
2026-09-19 22:35:53 +0000

    1 task 2E done

--- Class Exercises/1 - Introduction to Javascript/Task 2e - Car Hire/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2e - Car Hire/script.js
@@ -1,11 +1,17 @@
 // Add event listener to the calculate button
 document.getElementById('calculateBtn').addEventListener('click', calculateHireCharge);
-
+let charge = 0
+let total = 0
 function calculateHireCharge() {
     // TODO:
     // 1. Get the miles travelled from the input field
+    let miles = parseInt(document.getElementById("miles").value)
     // 2. Calculate:
     //    - Mileage charge (5p per mile = £0.05 per mile)
+    charge = miles * 0.05
     //    - Total charge (£25.00 + mileage charge)
+    total = charge + 25
     // 3. Display both charges formatted to 2 decimal places
+    document.getElementById("mileageCharge").textContent = + charge.toFixed(2)
+    document.getElementById("totalCharge").textContent = + total.toFixed(2)
 }
