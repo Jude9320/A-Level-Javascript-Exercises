c64fdb347dcf5d4cec69c5396cdfe9839368e07a
Jude9320 <628jk70@canonslade.tbft.uk>
2026-09-19 21:56:08 +0000

    1 task 2B done

--- Class Exercises/1 - Introduction to Javascript/Task 2b - Temperature Converter/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2b - Temperature Converter/script.js
@@ -1,9 +1,12 @@
 // Add event listener to the convert button
 document.getElementById('convertBtn').addEventListener('click', convertTemperature);
-
+let fah = 0
 function convertTemperature() {
     // TODO:
     // 1. Get the Celsius temperature from the input field
+    let cel = parseInt(document.getElementById("celsius").value)
     // 2. Convert to Fahrenheit using the formula: F = 1.8 * C + 32
+    fah = 1.8 * (cel + 32)
     // 3. Display the result in the fahrenheit span element
+    document.getElementById("fahrenheit").textContent = + fah
 }
