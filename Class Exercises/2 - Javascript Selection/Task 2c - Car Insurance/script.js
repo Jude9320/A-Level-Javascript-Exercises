8cd2afdad5aaab96d09c144be6051a757996359f
Jude9320 <628jk70@canonslade.tbft.uk>
2026-09-26 14:19:18 +0000

    2 task 2C done

--- Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2c - Car Insurance/script.js
@@ -7,7 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
     calculateButton.addEventListener('click', calculatePremium);
 });
 const basePremium = 500
-let discount = 0
+let discount = ""
+let premium = 0
+let claimsFee = 0
+let fee = 0
+let baseClaimsFee = 0
 // Function to calculate the insurance premium
 function calculatePremium() {
     // TODO: Get values from input fields
@@ -17,23 +21,42 @@ function calculatePremium() {
     // TODO: Calculate base premium (£500)
     // TODO: Apply age adjustment
     if (age < 25) {
-        discount = (basePremium * 0.5) + basePremium
+        premium = (basePremium * 0.5) + basePremium
+        fee = "+50%"
     }else if (age > 40) {
-        discount = basepremium * 0.85
+        premium = basePremium * 0.85
+        fee = "-15%"
     } else {
-        discount = basePremium
+        premium = basePremium
+        fee = "+0%"
     }
     // Under 25: +50%
     // 25-40: No change
-    // Over 40: -15%
-    
-    
+    // Over 40: -15% 
     // TODO: Apply no claims bonus
     // 0 years: No discount
+    if (claims === 0) {
+        premium = (premium * 1)
+        discount = "0%"
     // 1-2 years: 10% discount
+    } else if (claims >= 1 && claims <= 2){
+        premium = (premium * 0.9)
+        discount = "10%"
     // 3-5 years: 25% discount
-    // Over 5 years: 35% discount  
+    } else if (claims >= 3 && claims <= 5){
+        premium = (premium * 0.75)
+        discount = "25%"
+    // Over 5 years: 35% discount 
+    } else {
+        premium = (premium * 0.65)
+        discount = "35%"
+    }
     // TODO: Add previous claims adjustment
-    // Each claim adds 20%  
+    // Each claim adds 20%
+    baseClaimsFee = (premium * 0.2)
+    claimsFee = (baseClaimsFee * previousClaims)
     // TODO: Display the final premium and the breakdown of calculations
+    let finalPremium = (claimsFee + premium)
+    document.getElementById("breakdownSection").textContent = "Total previous claims fee = " + claimsFee + ". Total price change for age = " + fee + ". Total discount = " + discount
+    document.getElementById("result").textContent = finalPremium
 }
